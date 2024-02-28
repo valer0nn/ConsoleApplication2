66c65460ee26f5cdcdd7c7013fa5b5fbbde702ca
Valeriya Kosykh <valeriya.kosyh2001@gmail.com>
2024-02-28 14:18:11 +0300

    сумма

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,12 @@
 
 int main()
 {
-    int a=2,b=3, c,d;
+    int a=2,b=3, c,d,sum;
     c = a + b;
     std::cout << "c = " << c;
     d = a * b;
     std::cout << "d = " << d;
+    sum =c + d;
+    std::cout << "sum = " << sum;
 }
 
